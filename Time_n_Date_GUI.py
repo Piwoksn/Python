721108c1fde38c84e7c4e4a9ad18c06efa1a3e17
Piwoks Noble <101385603+Piwoksn@users.noreply.github.com>
2023-10-02 19:07:24 +0100

    Add files via upload
    Digital Clock

---
+++ Time_n_Date_GUI.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from time import *
+
+#function
+def update():
+    time_string = strftime("%I:%M:%S %p")
+    time_label.config(text= time_string)
+    
+    day_string = strftime("%A")
+    day_label.config(text= day_string)
+    
+    date_string= strftime("%B %d, %Y")
+    date_label.config(text= date_string)
+    
+    #time_label.after(1000, update)
+    window.after(1000, update)
+    
+
+window = Tk()
+
+#time
+time_label= Label(window, font= ('Arial', 20), fg = "#00FF00", bg= 'black')
+time_label.pack()
+
+#day
+day_label= Label(window, font= ('Ink Free', 15))
+day_label.pack()
+
+#date
+date_label= Label(window, font=('Ink Free', 15))
+date_label.pack()
+
+update()
+
+
+
+window.mainloop()
